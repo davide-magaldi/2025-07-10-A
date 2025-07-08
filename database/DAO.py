24c1473a559f49bb1d586dddadc75a90aeb0de2b
Giuseppe Averta <g.averta3@gmail.com>
2025-07-08 15:48:53 +0200

    provide a sample method in DAO

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,31 @@
 from database.DB_connect import DBConnect
 
+from model.product import Product
+from model.category import Category
+
 
 class DAO():
     def __init__(self):
         pass
+
+    @staticmethod
+    def getDateRange():
+
+        conn = DBConnect.get_connection()
+
+        results = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = "SELECT distinct (order_date) from orders o order by order_date"
+
+        cursor.execute(query)
+
+        for row in cursor:
+            results.append(row["order_date"])
+
+        first = results[0]
+        last = results[-1]
+
+        cursor.close()
+        conn.close()
+        return first, last
